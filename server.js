125ab24b8d6f232732f31cbff0876dbb8463303d
Andrea Greco <andreag.email@gmail.com>
2023-05-28 18:10:30 +0700

    server
    server compare

---
+++ server.js
@@ -0,0 +1,40 @@
+// Import required modules
+const http = require('http');
+const fs = require('fs');
+const pixelmatch = require('pixelmatch');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Handle requests
+  if (req.url === '/compare') {
+    // Load images and perform comparison
+    const img1 = fs.readFileSync('images/image1/triangles.png');
+    const img2 = fs.readFileSync('images/image2/triangles.png');
+    const { width, height } = getImageDimensions(img1); // Replace with your method to get image dimensions
+
+    const diff = new Uint8Array(width * height * 4); // Create an array to store diff data
+    const numDiffPixels = pixelmatch(img1, img2, diff, width, height, { threshold: 0.2 });
+
+    // Do something with the comparison result
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ numDiffPixels }));
+  } else {
+    // Handle other requests
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+});
+
+// Start the server
+const port = process.env.PORT || 3000; // Use the port provided by Render.com or fallback to 3000
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Helper function to get image dimensions
+function getImageDimensions(imageBuffer) {
+  // Implement your own logic to get image dimensions here
+  // You can use additional libraries like 'sharp' or 'jimp' for image processing if needed
+  // Return an object with 'width' and 'height' properties
+  return { width: 0, height: 0 };
+}
