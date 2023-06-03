abcaa9ecf95ac9932c7c336d4074b561db7d24db
Andrea Greco <andreag.email@gmail.com>
2023-06-03 12:27:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const pixelmatch = require('pixelmatch');
 // Create an HTTP server
 const server = http.createServer((req, res) => {
   // Handle requests
-  if (req.url === '/compare') {
+  if (req.url === '/compare' && req.method === 'GET') {
     // Load images and perform comparison
     const img1 = fs.readFileSync('images/image1/triangles.png');
     const img2 = fs.readFileSync('images/image2/triangles.png');
     const { width, height } = getImageDimensions(img1); // Replace with your method to get image dimensions
 
     const diff = new Uint8Array(width * height * 4); // Create an array to store diff data
-    const numDiffPixels = pixelmatch(img1, img2, diff, width, height, { threshold: 0.2 });
+    const numDiffPixels = pixelmatch(img1, img2, diff, width, height, { threshold: 0.1 });
 
     // Do something with the comparison result
     res.writeHead(200, { 'Content-Type': 'application/json' });
